ac6782ec27b989a73e9b4f1423718af728034ab7
laszlobalint1987@gmail.com <laszlobalint1987@gmail.com>
2018-10-26 21:28:02 +0200

    Initial commit

---
+++ standard_input.py
@@ -0,0 +1,9 @@
+# Introduce yourself
+name = input('Give me your name: ')
+age = input('... and your current age: ')
+print('Hello', name, 'who is', age, 'years old.')
+
+# Calculate the area of a circle
+radius = input('Enter the radius of the cirle (m): ')
+area = 3.143 * int(radius) ** 2
+print('The area of the circle is', area)
